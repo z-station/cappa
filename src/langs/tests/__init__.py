6ff55b5e04cfd114b91f277eedd77e4165686531
serious-notreally <61718385+serious-notreally@users.noreply.github.com>
2020-06-10 15:33:23 +0500

    deleted migrations

--- src/langs/tests/__init__.py
+++ src/langs/tests/__init__.py
@@ -1,9 +1,9 @@
-#from src.langs.providers.python.tests import ProviderTestCase as PythonTestCase
+from src.langs.providers.python.tests import ProviderTestCase as PythonTestCase
 from src.langs.providers.cpp.tests import ProviderTestCase as CppTestCase
 from src.langs.providers.csharp.tests import ProviderTestCase as CSharpTestCase
 from src.langs.providers.sql.tests import ProviderTestCase as SqlTestCase
 if __name__ == '__main__':
-    #PythonTestCase()
+    PythonTestCase()
     CppTestCase()
     CSharpTestCase()
     SqlTestCase()
