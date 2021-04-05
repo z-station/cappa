6a6251ddb537e36c5f207a4a438e6de04f55a5a5
azat.zakirov <zakirmalay@gmail.com>
2021-04-06 03:36:15 +0500

    93 feat(docker): оптимизация сборки

--- docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-memcached -d -u memcache
-cd /app/src
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn --bind 0:9000 app.wsgi:application --reload -w ${APP_GUNICORN_WORKERS:=1}
